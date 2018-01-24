1101c1b5197ad31ab357473c42a3ef0881417d63
olgabot <olga.botvinnik@gmail.com>
2018-01-24 15:16:54 -0800

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 from setuptools import setup
 
-version = '0.1.0'
+version = '0.1.1'
 
 required = open('requirements.txt').read().split('\n')
 
 setup(
     name='hermione',
     version=version,
-    description='Joyplots and more for single cells',
+    description='Joyplots in Python with seaborn-style interface',
     author='Olga Botvinnik',
     author_email='olga.botvinnik@gmail.com',
     url='https://github.com/czbiohub/hermione',
