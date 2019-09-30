628be0ac07576da8b7e04787090da95d5a7e6bb3
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-09-30 16:52:08 -0700

    Okay try THIS to set outline kwargs

--- hermione/horizonplot.py
+++ hermione/horizonplot.py
@@ -43,7 +43,9 @@ def horizonplot(data, x, row, row_order=None, palette=None,
     facet_kws = FACET_KWS if facet_kws is None else _set_defaults(facet_kws, FACET_KWS)
     kdeplot_kws = KDEPLOT_KWS if kdeplot_kws is None else _set_defaults(kdeplot_kws, KDEPLOT_KWS)
     hline_kws = HLINE_KWS if hline_kws is None else _set_defaults(hline_kws, HLINE_KWS)
-    outline_kws =_set_defaults(OUTLINE_KWS.update(color=outline_color), KDEPLOT_KWS)
+    outline_kws = dict(color=outline_color)
+    outline_kws.update(OUTLINE_KWS)
+    outline_kws = _set_defaults(outline_kws, KDEPLOT_KWS)
 
     # Pad xlabel suffix with spaces
     xlabel_suffix = ' ' + xlabel_suffix if xlabel_suffix is not None else ''
